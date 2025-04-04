e39576e8f6eb103e2d5946413747ec98c4417564
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-04-04 19:00:13 -0300

    refactor: enhance Header and Hero components with new layout and ButtonCustom integration

--- src/components/landing_page/hero.tsx
+++ src/components/landing_page/hero.tsx
@@ -1,5 +1,6 @@
 import { assets } from "@/assets/assets";
 import Header from "../commons/Header";
+import ButtonCustom from "../commons/ButtonCustom";
 
 export default function Hero() {
     return (
@@ -13,7 +14,18 @@ export default function Hero() {
         >
             <Header />
 
-            <div></div>
+            <div className="absolute left-[10%] h-full flex flex-col justify-center">
+                <p className="text-6xl text-white font-light tracking-[1px] max-w-2xl mb-10">
+                    O Futuro da sua proxima refeição. 1º app do Brasil que te
+                    ajuda a escolher o que comer.
+                </p>
+
+                <ButtonCustom
+                    title="Baixe o app"
+                    color="bg-green-600"
+                    border="w-[200px] rounded-md elevation-2"
+                />
+            </div>
         </section>
     );
 }
