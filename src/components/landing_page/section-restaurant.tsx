3625f61231eb1edb30aadffe96d258066ab9c5f9
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-04-11 19:05:12 -0300

    feat: add SectionMaoNaMassa component and integrate carousel in SectionRestaurant

--- src/components/landing_page/section-restaurant.tsx
+++ src/components/landing_page/section-restaurant.tsx
@@ -1,4 +1,12 @@
 import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+import { carouselImages } from "../../assets/assets";
+
+type CarouselImage = {
+    src: string;
+    alt: string;
+};
 
 export default function SectionRestaurant() {
     return (
@@ -8,7 +16,37 @@ export default function SectionRestaurant() {
                     Os melhores restaurantes da cidade
                 </h2>
 
-                <Button variant="ghost">Ver todos</Button>
+                <Button
+                    variant="ghost"
+                    className=" text-xl text-muted-foreground"
+                >
+                    Ver todos
+                </Button>
+            </div>
+
+            <div className="mt-16">
+                <Carousel className="w-full max-w-7xl">
+                    <CarouselContent>
+                        {carouselImages.map((image: CarouselImage, index: number) => (
+                            <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                                <div className="p-2">
+                                    <Card>
+                                        <CardContent className="flex items-center justify-center">
+                                            <img 
+                                            src={image.src}
+                                            alt={image.alt} 
+                                            className="h-64 w-full object-cover rounded-lg shadow-md transition-transform duration-600 transform hover:scale-105"
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </div>
         </section>
     );
