f4f60ca4679c01ec268bb7cf7c43c95788d21674
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-06-26 05:34:30 -0300

    style: enhance button styling in FooterCard component for improved visibility

--- src/components/commons/footer-card.tsx
+++ src/components/commons/footer-card.tsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaGoogle, FaMicrosoft } from "react-icons/fa";
 export default function FooterCard() {
     return (
         <div className=" flex flex-row items-center justify-center w-full mt-4 gap-5">
-            <Button className="rounded-lg" variant="outline">
+            <Button className="rounded-lg border border-green-600 hover:bg-green-100" variant="outline">
                 <FaGoogle className="w-7 h-7 text-gray-700" />
             </Button>
 
